8311414d7e994fa4862468f6db4c535efa1fcaa7
The Lord of Owls <119661891+The-Lord-of-Owls@users.noreply.github.com>
2023-07-29 00:41:32 -0400

    Update bulletlib.lua
    Bill Nye suggested some optimizations

--- lua/includes/modules/bulletlib.lua
+++ lua/includes/modules/bulletlib.lua
@@ -6,43 +6,16 @@ local Projectiles = {}
 local ArmorMaterials = {}
 
 
---[[
-	9mm example
-	Velocity: 900
-	Weight: 115
-	Burn Time: 7
-	Target Resistance: 50
-	Damage: 45
-		
-	5.56 nato example
-	velocity: 2750
-	weight: 55
-	Burn Time: 14.5
-	Target Resistance: 50
-	Damage: 1902.5
-	
-	This highlights just how important bullet resistence is. Think of it as you would body armor. You still feel the impact even if it doesn't penetrate
-	and even if it does penetrate, some of the bullets power is lost after going through the armor. Obviously higher power rounds will punch through armor and retain more damage
-	
-	Velocity in these examples above is the lowest reported fps for real world ammo
-	weight is based on lowest real world bullet grain weight
-	Burn Time is the optimal barrel length to ensure all powder has been burned before the round leaves the barrel
-	Target resistence is the balistic resistence of a target
-	
-	damage calculation formula:
-		( Velocity - Weight * BurnTime ) - Resistance
-		
-	This is not the MOST realistic way to determine damage for bullets, but it is close while still being reasonable. If we find a better way later, then this will change
-]]
 function RegisterProjectile( name, velocity, weight, burnTime )
 	--Registering new projectile
-	Projectiles[ name ] = velocity - weight * burnTime,
+	Projectiles[ name ] = velocity - weight * burnTime
 	
 	--Handle tracer related stuff so that we can have our tracers ready to go when ever needed
 end
 
 function RegisterArmorMaterial( name, resistence )
 	--Registering new armor material
+	--todo: Incorperate real world values for the armor material
 	ArmorMaterials[ name ] = resistence
 end
 
@@ -61,7 +34,7 @@ function FireBullet( ent, bullet )
 		TracerName = bullet
 	}
 
-	--setup lagcompensation here
+	
 	if shouldLagComp then owner:LagCompensation( true ) end
 	ent:FireBullets( bulletInfo )
 	if shouldLagComp then owner:LagCompensation( false ) end
@@ -70,11 +43,7 @@ end
 
 --Metafunctions for getting/setting armor type and getting bullet resistence based on player's armor type
 function plyMeta:GetBulletResistence()
-	if self.armorType then
-		return ArmorMaterials[ self.armorType ] or 0
-	end
-
-	return 0
+	return self.armorType and ArmorMaterials[ self.armorType ] or 0
 end
 
 function plyMeta:GetArmorType()
@@ -86,20 +55,18 @@ function plyMeta:SetArmorType( name )
 end
 
 
+if CLIENT then return end
+local initialized = false
+
 --Handle armor resistence, should also work with weapons that don't use our damage calculations. May result in other weapons doing hilariously low damage or possibly healing targets. Who knows
-if SERVER then
-	local initialized = false
+function Initialize()
+	if initialized then return end
+	initialized = true
 
-	function Initialize()
-		if initialized then return end
-		initialized = true
-
-		hook.Add( "EntityTakeDamage", "BalisticArmorCheck", function( targetm, dmginfo )
-			if target:IsPlayer() and dmginfo:IsBulletDamage() then
-				dmginfo:SubtractDamage( target:GetBulletResistence() )
-			end
-		end)
-	end
+	hook.Add( "EntityTakeDamage", "BalisticArmorCheck", function( target, dmginfo )
+		if not target:IsPlayer() or not dmginfo:IsBulletDamage() then return end
+		dmginfo:SubtractDamage( target:GetBulletResistence() )
+	end)
 end
 
 
